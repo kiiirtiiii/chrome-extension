1f455358a52320082438ce3c76e5f9d56ec485fd
kiiirtiiii <kirtivats1409@gmail.com>
2023-08-26 01:46:30 +0530

    conflict resolved

--- front-end/apis.js
+++ front-end/apis.js
@@ -6,12 +6,20 @@ const apiBaseUrl = 'http://localhost:4000/api/'
 // Define the function to fetch summary by api call
 const fetchSummary = async (apiBaseUrl) => {
     try{
+        document.getElementById('loader').style.display = 'block'; // Show loader
+
         let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+        
         const response = await fetch(`${apiBaseUrl}/summary?url=${tab.url}&response_token=500`);
         const data = await response.json();
-        console.log(data);
-    }catch(err){
-        console.log(err);
+
+        document.getElementById('loader').style.display = 'none'; // Hide loader
+
+        // Display the result
+        document.getElementById('apiResponse').innerHTML = JSON.stringify(data.data);        
+    } catch(err){
+        document.getElementById('loader').style.display = 'none'; // Hide loader in case of error
+        console.log('Error: ',err);
     } 
 }
 
